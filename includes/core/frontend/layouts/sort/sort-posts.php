e60127ea62356353c7ad7f126917bcd62523c38e
jovaann <jovanprogram@gmail.com>
2024-04-23 09:52:09 +0200

    Changes in Sort filter

--- includes/core/frontend/layouts/sort/sort-posts.php
+++ includes/core/frontend/layouts/sort/sort-posts.php
@@ -9,8 +9,8 @@ echo '<span class="name-sort">'.esc_html__($sort_text, 'ymc-smart-filter').'</sp
 echo '</div>';
 echo '<div class="menu-passive">';
 echo '<i class="btn-close">x</i>';
-$criteria_sort_post =  '<div class="menu-passive__item"><a class="menu-link" data-order="'.esc_attr('desc').'" data-orderby="'.esc_attr('title').'" href="#">'.esc_html__('Sort by title', 'ymc-smart-filter').'</a></div>';
-$criteria_sort_post .= '<div class="menu-passive__item"><a class="menu-link" data-order="'.esc_attr('desc').'" data-orderby="'.esc_attr('date').'" href="#">'.esc_html__('Sort by publication date', 'ymc-smart-filter').'</a></div>';
+$criteria_sort_post =  '<div class="menu-passive__item"><a class="menu-link" data-order="'.esc_attr('desc').'" data-orderby="'.esc_attr('title').'" href="#">'.esc_html__('Title', 'ymc-smart-filter').'</a></div>';
+$criteria_sort_post .= '<div class="menu-passive__item"><a class="menu-link" data-order="'.esc_attr('desc').'" data-orderby="'.esc_attr('date').'" href="#">'.esc_html__('Date', 'ymc-smart-filter').'</a></div>';
 echo apply_filters('ymc_sort_posts_by_'.$id.'_'.$c_target, $criteria_sort_post);
 echo '</div>';
 echo '</div>';
